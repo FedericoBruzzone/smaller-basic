be3a6eb574e1ef8f0ac5b404b7ffdcba86829db6
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-06-28 01:24:41 +0200

    Add lexer to `Interpreter` class
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- src/interpreter/Interpreter.py
+++ src/interpreter/Interpreter.py
@@ -1,15 +1,49 @@
-from src.grammar import SmallerBasicLexer
-from src.grammar import SmallerBasicParser
-
 import sys
 from typing import Any
 
+from antlr4.InputStream import InputStream
+
+from src.grammar import SmallerBasicLexer
+from src.grammar import SmallerBasicParser
+
 class Interpreter(object):
     def __init__(self):
-        self.__source_code: Any = None
-    
-    def load_file(self, file_path: str):
-        with open(file_path, encoding="UTF-8", mode="r") as file:
-            self.__source_code = file.read().splitlines()
-        print(self.__source_code)
+        self.__source_code: Any          = None
+        self.__is_running: bool          = False
 
+    def load_file(self, file_path: str) -> None:
+        """
+        Load the source code from a file and split it into lines.
+
+        Args:
+            file_path (str): The path to the source code file.
+        """
+        with open(file_path, encoding="UTF-8", mode="r") as file:
+            self.__source_code = file.read()
+
+        if self.__source_code is not None:
+            self.split_source_code(self.__source_code)
+
+    def split_source_code(self, source_code: str) -> None:
+        """
+        Split the source code into lines.
+        """
+        self.source_code = source_code.splitlines()
+   
+
+    def run(self, file_path):
+        """
+        Run the interpreter.
+
+        Args:
+            file_path (str): The path to the source code file.
+        """
+        self.load_file(file_path)
+
+        if self.__source_code is not None:
+            self.__is_running = True
+        
+        input_stream: InputStream = InputStream(self.__source_code)
+        lexer: SmallerBasicLexer = SmallerBasicLexer(input_stream)) 
+        
+     
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from src.interpreter.Interpreter import Interpreter 
 import sys
+
+from src.interpreter.Interpreter import Interpreter 
 
 if __name__ == "__main__":
     interpreter: Interpreter = Interpreter()
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
-        interpreter.load_file(file_path)
+        interpreter.run(file_path)
     else:
         print("No file path provided.")
